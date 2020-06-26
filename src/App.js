667f995b70a826525e02b0e72640cc3d1b0255e1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-06-25 17:55:41 -0700

    Updated theme in material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import { lightBlue } from '@material-ui/core/colors';
 import Header from './components/Header'
 import Item from './components/Item'
 import Cart from './components/Cart'
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+      primary: {
+        light: '#63ccff',
+        main: '#039be5',
+        dark: '006db3'
+      }
+  }
+});
+
+console.log(lightBlue);
+
+
 function App() {
   return (
+    <MuiThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <Header />
@@ -13,6 +29,7 @@ function App() {
         <Cart />
       </header>
     </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
 
 function Header() {
 
     return (
+        
         <div>
             <h3>Store</h3>
+            <Button variant="contained" color="primary" className="nav-button"><span className="text-button">Store</span></Button>
         </div>
     );
 
