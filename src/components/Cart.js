b4b2089f2e7b3849626c80b06ecfccbc9dece2a5
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-06-26 14:52:35 -0700

    Added material-ui icons package

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 
 function Cart() {
+
+    // const [cartItems, setCartItems] = useState([]);
+
+    // function addCartItem() {
+    //     setCartItems(previousItems => {
+    //         return [...previousItems]
+    //     })
+    // };
     
     return (
     <div>
