5b2d36175366be401a6241c74d0f9781fb89d7af
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-06-28 07:42:47 -0700

    Updated card styling and item data through props

--- src/components/ContainerDisplay.js
+++ src/components/ContainerDisplay.js
@@ -10,6 +10,7 @@ const items = [
         name: 'Assault Rifle',
         model: 'AK47_20K_HP.FBX',
         description: 'This is a 3D Model of an AK-47.',
+        img: 'akm-ak-47-ak-74-01.jpg',
         poly: '20K',
         rigged: true,
         price: 24.99
@@ -19,6 +20,7 @@ const items = [
         name: 'Kitchen Utensils',
         model: 'Kitchen_Utensils_5K_LP.FBX',
         description: '3D Model of a spoon, fork, knife, glass, mug, bowl, & plate',
+        img: 'w3ixvwkx-900.jpg',
         poly: '5K',
         rigged: false,
         price: 2.99
@@ -28,6 +30,7 @@ const items = [
         name: 'Leather Sofa',
         model: 'Leather_Sofa_12K_HP.FBX',
         description: 'This is a 3D Model of a Leather Sofa.',
+        img: 'savoy-leather-sofas.jpg',
         poly: '12K',
         rigged: false,
         price: 13.99
@@ -42,31 +45,31 @@ function ContainerDisplay() {
     <div>
         <Grid container justify="center" spacing={2}>
             <Grid item xs={4}>
-                <Item title={items[0].name} description={items[0].description} price={items[0].price}></Item>
+                <Item image={items[0].img} title={items[0].name} description={items[0].description} price={items[0].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[1].name} description={items[1].description} price={items[1].price}></Item>
+                <Item image={items[1].img} title={items[1].name} description={items[1].description} price={items[1].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[2].name} description={items[2].description} price={items[2].price}></Item>
+                <Item image={items[2].img} title={items[2].name} description={items[2].description} price={items[2].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[0].name} description={items[0].description} price={items[0].price}></Item>
+                <Item image={items[0].img} title={items[0].name} description={items[0].description} price={items[0].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[1].name} description={items[1].description} price={items[1].price}></Item>
+                <Item image={items[1].img} title={items[1].name} description={items[1].description} price={items[1].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[2].name} description={items[2].description} price={items[2].price}></Item>
+                <Item image={items[2].img} title={items[2].name} description={items[2].description} price={items[2].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[0].name} description={items[0].description} price={items[0].price}></Item>
+                <Item image={items[0].img} title={items[0].name} description={items[0].description} price={items[0].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[1].name} description={items[1].description} price={items[1].price}></Item>
+                <Item image={items[1].img} title={items[1].name} description={items[1].description} price={items[1].price}></Item>
             </Grid>
             <Grid item xs={4}>
-                <Item title={items[2].name} description={items[2].description} price={items[2].price}></Item>
+                <Item image={items[2].img} title={items[2].name} description={items[2].description} price={items[2].price}></Item>
             </Grid>
             
         </Grid>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,27 @@
 import React from "react";
-import Card from '@material-ui/core/Card'
+import Card from "@material-ui/core/Card";
+import { CardMedia, CardContent, Typography, Button, CardActionArea } from "@material-ui/core";
 
+function Item(props) {
 
-
-function Item (props) {
-    //console.log(props.name);
-    return (
-    <div>
-        <Card>
-        <img className="item-img-card" src=""></img>
-        <p className="item-title-card">{props.title}</p>
-        <p className="item-description-card">{props.description}</p>
-        <p className="item-price-card">{props.price}</p>
+  return (
+    <div className="item-card">
+      <Card>
+      <CardActionArea>
+          <CardMedia>
+            <img className="item-img-card" src={`../../images/${props.image}`} alt="3D model"></img>
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              <p className="item-title-card">{props.title}</p>
+              <p className="item-description-card">{props.description}</p>
+              <p className="item-price-card">{props.price}</p>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
         </Card>
     </div>
-    );
+  );
 }
 
 export default Item;
