07db82e28bcf8bb061995450fa0930f268b64b72
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-07-15 18:16:34 -0700

    Updated styling

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
-    return res.send('pong');
+app.get("/ping", function (req, res) {
+  return res.send(`<h1>Pong</h1>`);
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.js'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.js"));
 });
 
-app.listen(process.env.PORT || 3000);
+app.get("/item", (req, res) => {
+    
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('Server running on port: ', port);
+});
